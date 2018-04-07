43af739073b4c4c2ad1e0c221db96ef5db931fd5
Olav Vahtras <vahtras@kth.se>
2018-04-07 03:39:24 +0200

    Generalize categories

--- click.py
+++ click.py
@@ -4,38 +4,43 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
-def main(df):
-    y = range(2)
+def main(df, x, category):
+    category_values = sorted(list(df[category].unique()))
+    y = range(len(category_values))
     #print(df)
 
     fig, ax = plt.subplots()
-    sns.boxplot(data=df, x=x, y=ab, whis=(10, 90), orient='h', order=('A', 'B'))
+    sns.boxplot(data=df, x=x, y=category, whis=(10, 90), orient='h',
+        order=category_values
+        )
 
     def onclick(event):
         if event.xdata is not None and event.ydata is not None:
             is_near = (df.x - event.xdata)**2 < .01
-            is_A = (df.ab == "A") & (event.ydata < .1)
-            sel = is_A & is_near
-            if sel.any():
-                #print(df[sel])
-                for i, row in df[sel].iterrows():
-                    ax.annotate(
-                        f"{row.ab}\n{row.x}",
-                        xy=(row.x, y[0]),
-                        xytext=(20, 20),
-                        textcoords="offset points",
-                        bbox={"boxstyle": "round", "fc": "w", "lw": 2},
-                        arrowprops={'arrowstyle': '->'},
-                    )
-                fig.canvas.draw_idle()
+            for iv, cv in enumerate(category_values):
+                is_category = (df[category] == cv) & (
+                abs(event.ydata-y[iv]) < .1
+                )
+                sel = is_category & is_near
+                if sel.any():
+                    #print(df[sel])
+                    for i, row in df[sel].iterrows():
+                        ax.annotate(
+                            f"{row[category]}\n{row[x]}",
+                            xy=(row[x], y[iv]),
+                            xytext=(20, 20),
+                            textcoords="offset points",
+                            bbox={"boxstyle": "round", "fc": "w", "lw": 2},
+                            arrowprops={'arrowstyle': '->'},
+                        )
+                    fig.canvas.draw_idle()
 
     cid = fig.canvas.mpl_connect('motion_notify_event', onclick)
-    fig.suptitle(', '.join(map(str, sorted(x))))
     plt.show()
 
 if __name__ == "__main__":
     x = np.random.randint(1, 100, 20)
     ab = np.random.choice(list('AB'), 20)
-    df = pd.DataFrame(dict(x=x, ab=ab))
+    df = pd.DataFrame(dict(x=x, category=ab))
 
-    sys.exit(main(df))
+    sys.exit(main(df, 'x', 'category'))
