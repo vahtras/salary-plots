8946ff5b97b6b5f3c4fd238ddf98aca59e766d9f
Olav Vahtras <vahtras@kth.se>
2018-04-07 02:46:04 +0200

    rewrite for annotation

--- click.py
+++ click.py
@@ -1,26 +1,38 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 import numpy as np
 
+XEPS = 0.5
+YEPS = 0.01
+
 x = np.random.randint(1, 100, 20)
-y = 1
+ab = np.random.choice(list('AB'), 20)
+y1, y2 = range(2)
+df = pd.DataFrame(dict(x=x, ab=ab))
+#print(df)
 
 fig, ax = plt.subplots()
-ax.boxplot(x, vert=False, whis=(10,90))
+sns.boxplot(data=df, x=x, y=ab, whis=(10, 90), orient='h', order=('A', 'B'))
 
 def onclick(event):
-    #print(f"x={event.x} y={event.y} button={event.button}")
-    #print(f"    xdata={event.xdata} ydata={event.ydata}")
     if event.xdata is not None and event.ydata is not None:
-        ix = int(round(event.xdata))
-        if ix in x and abs(event.ydata - y) < .01:
-            print(f"Picked x={ix} y={event.ydata}")
-            ax.annotate(
-                f"{ix}", xy=(ix, y), xytext=(20, 20),
-                textcoords="offset points",
-                bbox={"boxstyle": "round", "fc": "w"},
-                arrowprops={'arrowstyle': '->'},
+        is_near = (df.x - event.xdata)**2 < .01
+        is_A = (df.ab == "A") & (event.ydata < .1)
+        sel = is_A & is_near
+        if sel.any():
+            #print(df[sel])
+            for i, row in df[sel].iterrows():
+                ax.annotate(
+                    f"{row.ab}\n{row.x}",
+                    xy=(row.x, y1),
+                    xytext=(20, 20),
+                    textcoords="offset points",
+                    bbox={"boxstyle": "round", "fc": "w", "lw": 2},
+                    arrowprops={'arrowstyle': '->'},
                 )
             fig.canvas.draw_idle()
 
 cid = fig.canvas.mpl_connect('motion_notify_event', onclick)
+fig.suptitle(', '.join(map(str, sorted(x))))
 plt.show()
