02e5e74047a3ed0031d09a8b37cb3b0f35d78bbe
Olav Vahtras <vahtras@kth.se>
2018-04-08 15:32:57 +0200

    two subcategories ok

--- click.py
+++ click.py
@@ -4,44 +4,63 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
-def main(df, x, category, annotate=None):
+def set_y(df, y_label, y_sublabel=None):
+    labels = sorted(list(df[y_label].unique()))
+    y_values = (labels.index(row[y_label]) for _, row in df.iterrows())
+    df["y"] = pd.Series(y_values)
+    if y_sublabel:
+        sublabels = sorted(list(df[y_sublabel].unique()))
+        y_subvalues = (
+            sublabels.index(row[y_sublabel]) for _, row in df.iterrows()
+        )
+        df["dy"] = pd.Series(y_subvalues)
+        df.dy -= (len(sublabels)-1) / 2
+        df.dy *= .4
+        df.y += df.dy
+        
+
+def main(df, x, category, hue=None, annotate=None):
     category_values = sorted(list(df[category].unique()))
-    y = range(len(category_values))
-    #print(df)
+    hue_values = None
+    if hue:
+        hue_values = sorted(list(df[hue].unique()))
+    set_y(df, category, hue)
 
     fig, ax = plt.subplots()
-    sns.boxplot(data=df, x=x, y=category, whis=(10, 90), orient='h',
-        order=category_values
+    sns.boxplot(
+        data=df, x=x, y=category, hue=hue,
+        whis=(10, 90),
+        orient='h',
+        order=category_values,
+        hue_order=hue_values
         )
 
     def onclick(event):
         if event.xdata is not None and event.ydata is not None:
-            is_near = (df[x] - event.xdata)**2 < 1000
-            for iv, cv in enumerate(category_values):
-                is_category = (df[category] == cv) & (
-                abs(event.ydata-y[iv]) < .1
-                )
-                sel = is_category & is_near
-                if sel.any():
-                    for i, row in df[sel].iterrows():
-                        ax.annotate(
-                            "\n".join(str(row[k]) for k in annotate),
-                            xy=(row[x], y[iv]),
-                            xytext=(20, 20),
-                            textcoords="offset points",
-                            bbox={"boxstyle": "round", "fc": "w", "lw": 2},
-                            arrowprops={'arrowstyle': '->'},
-                        )
-                    fig.canvas.draw_idle()
+            is_near_x = (df[x] - event.xdata)**2 < 1000
+            is_near_y = (df.y - event.ydata)**2 < 0.1
+            selected = is_near_x & is_near_y
+            if selected.any():
+                for i, row in df[selected].iterrows():
+                    ax.annotate(
+                        "\n".join(str(row[k]) for k in annotate),
+                        xy=(row[x], row.y),
+                        xytext=(20, 20),
+                        textcoords="offset points",
+                        bbox={"boxstyle": "round", "fc": "w", "lw": 2},
+                        arrowprops={'arrowstyle': '->'},
+                    )
+                fig.canvas.draw_idle()
 
     if annotate is not None:
         cid = fig.canvas.mpl_connect('motion_notify_event', onclick)
     plt.show()
 
 if __name__ == "__main__":
-    x = np.random.randint(20000, 40000, 20)
-    km = np.random.choice(['Kvinna', 'Man'], 20)
-    school = np.random.choice(['A', 'B', 'C'], 20)
-    df = pd.DataFrame(dict(x=x, Skola=school))
+    sample = 100
+    x = np.random.randint(20000, 40000, sample)
+    km = np.random.choice(['Kvinna', 'Man'], sample)
+    school = np.random.choice(['A', 'B', 'C'], sample)
+    df = pd.DataFrame(dict(x=x, Skola=school, Kön=km))
 
-    sys.exit(main(df, 'x', 'Skola', annotate=('Skola', 'x')))
+    sys.exit(main(df, 'x', 'Skola', 'Kön', annotate=('Skola', 'x', 'Kön')))
---
+++ test_ab.py
@@ -0,0 +1,44 @@
+import pytest
+import numpy as np
+import pandas as pd
+import pandas.util.testing as pdt
+import click
+
+"""
+      Kön Skola      x
+0  Kvinna     B  22732
+1  Kvinna     B  30799
+2  Kvinna     B  29845
+3     Man     A  39648
+4  Kvinna     B  33123
+5     Man     A  29225
+6     Man     A  34116
+7  Kvinna     B  34935
+8  Kvinna     C  35430
+9     Man     A  35832
+"""
+
+
+@pytest.fixture
+def sample():
+    samples = 10
+    np.random.seed(0)
+    x = np.random.randint(20000, 40000, samples)
+    km = np.random.choice(['Kvinna', 'Man'], samples)
+    school = np.random.choice(['A', 'B', 'C'], samples)
+    df = pd.DataFrame(dict(x=x, Skola=school, Kön=km))
+    return df
+
+def test_init(sample):
+    assert len(sample) == 10
+
+def test_y_cat(sample):
+    click.set_y(sample, 'Skola')
+    assert  list(sample.y) == [1., 1., 1., 0., 1., 0., 0., 1., 2. ,0.]
+
+def test_y_subcat(sample):
+    click.set_y(sample, 'Skola', 'Kön')
+    pdt.assert_series_equal(
+        sample.y, pd.Series([.8, .8, .8, 0.2, .8, .2, .2, .8, 1.8 ,.2], name='y')
+    )
+
