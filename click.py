744a6e4486ba99e2e12baba5b5187d12e2f2b792
Olav Vahtras <vahtras@kth.se>
2018-04-24 06:28:50 +0200

    backup

--- click.py
+++ click.py
@@ -7,7 +7,7 @@ import numpy as np
 def set_y(df, y_label, y_sublabel=None):
     _df = df.copy()
     y_labels = sorted(list(_df[y_label].unique()))
-    y_values = [y_labels.index(row[y_label]) for _, row in _df.iterrows()]
+    y_values = [y_labels.index(row[y_label]) if pd.notnull(row[y_label])  else -1 for _, row in _df.iterrows() ]
     _df["y"] = y_values
     if y_sublabel:
         y_sublabels = sorted(list(_df[y_sublabel].unique()))
@@ -32,25 +32,25 @@ def get_row(event, df):
             return df[selected].iloc[0]
         
 def hover(event):
-    if event.xdata is not None and event.ydata is not None:
-        is_near_x = (hover._df.x - event.xdata)**2 < 10000
-        is_near_y = (hover._df.y - event.ydata)**2 < 0.1
-        selected = is_near_x & is_near_y
-        if selected.any():
-            fig = plt.gcf()
-            ax = plt.gca()
-            for i, row in hover._df[selected].iterrows():
-                ax.annotate(
-                    "\n".join(f"{row[k]}" for k in hover._annotate if pd.notnull(row[k]) and row[k] != 0),
-                    xy=(row.x, row.y),
-                    xytext=(20, 20),
-                    textcoords="offset points",
-                    bbox={"boxstyle": "square", "fc": "w", "lw": 2, "pad": 0.6},
-                    arrowprops={'arrowstyle': '->'},
-                )
-            fig.canvas.draw_idle()
+    row = get_row(event, hover._df)
+    if row is not None:
+        fig = plt.gcf()
+        ax = plt.gca()
+        ax.annotate(
+            "\n".join(f"{row[k]}" for k in hover._annotate if pd.notnull(row[k]) and row[k] != 0),
+            xy=(row.x, row.y),
+            xytext=(20, 20),
+            textcoords="offset points",
+            bbox={"boxstyle": "square", "fc": "w", "lw": 2, "pad": 0.6},
+            arrowprops={'arrowstyle': '->'},
+        )
+        fig.canvas.draw_idle()
 
-def main(df, x, category, hue=None, annotate=None, on_hover=hover, on_click=ignore):
+def main(
+        df, x, category, hue=None, annotate=None,
+        on_hover=hover, on_click=ignore,
+        **kwargs
+        ):
     category_values = sorted(list(df[category].dropna().unique()))
     hue_values = None
     if hue:
@@ -62,8 +62,23 @@ def main(df, x, category, hue=None, annotate=None, on_hover=hover, on_click=igno
         whis=(10, 90),
         orient='h',
         order=category_values,
-        hue_order=hue_values
+        hue_order=hue_values,
         )
+    ax.set_title(kwargs.get('title', None))
+    vlines = kwargs.get('vlines', None)
+    if vlines:
+        _, ymax = plt.ylim()
+        for what, where in vlines.items():
+            ax.axvline(where, color='g')
+            #xticks.append(x)
+            #xlabels.append(labx)
+            ax.annotate(f"{what}", (where, ymax), rotation=0)
+    text = kwargs.get('text', None)
+    if text:
+        props = dict(boxstyle='round', facecolor='white', alpha=0.5)
+        ax.text(1.05, 0.50, text, transform=ax.transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
+
 
     if annotate is not None:
         on_hover._df = set_y(df, category, hue)
@@ -74,15 +89,27 @@ def main(df, x, category, hue=None, annotate=None, on_hover=hover, on_click=igno
     on_click._df = on_hover._df
     fig.canvas.mpl_connect('button_press_event', on_click)
 
-    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
-    sample = 100
+    sample = 10
     x = np.random.randint(20000, 40000, sample)
     km = np.random.choice(['Kvinna', 'Man'], sample)
     school = np.random.choice(['A', 'B', 'C'], sample)
-    df = pd.DataFrame(dict(x=x, Skola=school, Kön=km))
+    oldschool = np.random.choice(['a', 'b', 'c'], sample)
+
+    uid1 = np.arange(sample)
+    uid2 = uid1.copy()
+    np.random.shuffle(uid2)
+
+    df = pd.DataFrame(dict(x=x, Skola=school, Kön=km, uid=uid1))
+    print(df)
+
+    df2 = pd.DataFrame(dict(Skola=oldschool, uid=uid2))
+    print(df2)
+    print(pd.merge(df, df2, on='uid'))
+    #raise SystemExit
+
 
     def print_xy(event):
         print(event.xdata, event.ydata)
@@ -93,7 +120,7 @@ if __name__ == "__main__":
 
     sys.exit(
         main(
-            df[df.Skola.isin(['B', 'C'])],
+            df,
             'x', 'Skola', 'Kön',
             annotate=('Skola', 'x', 'Kön'),
             on_click=print_row
