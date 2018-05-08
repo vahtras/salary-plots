29558e9cc75f241672a8d22b43afa46d5cc21ecc
Olav Vahtras <vahtras@kth.se>
2018-05-08 07:42:37 +0200

    backup

--- click.py
+++ click.py
@@ -7,24 +7,25 @@ import numpy as np
 class Plotter:
     def __init__(
         self, df, x, 
-        category=None, hue=None, on_hover=None, on_click=None, annotate=None
+        category=None, hue=None, on_hover=None, on_click=None, annotate=()
         ):
         self.df = df
         self.x = x
         self.category = category
         self.hue = hue
         self.on_hover = on_hover
-        self.on_click = on_click
+        self.on_click = print_xy
         self.annotate = annotate
-        if category is None:
-            self.df["y"] = 0.0
-        else:
-            self.df["y"] = self.set_y()
+        self.df["y"] = self.set_y()
 
     def category_values(self):
+        if self.category is None:
+            return []
         return sorted(list(self.df[self.category].dropna().unique()))
 
     def hue_values(self):
+        if self.hue is None:
+            return []
         return sorted(list(self.df[self.hue].dropna().unique()))
 
     def boxplot(self):
@@ -40,7 +41,7 @@ class Plotter:
 
     def get_row(self, event):
         if event.xdata is not None and event.ydata is not None:
-            is_near_x = (self.df.x - event.xdata)**2 < 10000
+            is_near_x = (self.df[self.x] - event.xdata)**2 < 10000
             is_near_y = (self.df.y - event.ydata)**2 < 0.1
             selected = is_near_x & is_near_y
             if selected.any():
@@ -48,7 +49,7 @@ class Plotter:
 
     def set_y(self):
         if self.category is None:
-            return [0]
+            return pd.Series(len(self.df)*[.0])
         y_labels = sorted(list(self.df[self.category].unique()))
         y_values = pd.Series(
             y_labels.index(row[self.category]) 
@@ -74,10 +75,7 @@ class Plotter:
             return pd.Series(len(self.df)*[0.])
 
     def __call__(self, event):
-        if True:
-            row = self.get_row(event)
-        else:
-            row = get_row_pp(event, hover._df)
+        row = self.get_row(event)
         if row is not None:
             fig = plt.gcf()
             ax = plt.gca()
@@ -87,135 +85,13 @@ class Plotter:
                         for k in self.annotate
                         if pd.notnull(row[k]) and row[k] != 0
                 ),
-                xy=(row.x, row.y),
+                xy=(row[self.x], row.y),
                 xytext=(20, 20),
                 textcoords="offset points",
                 bbox={"boxstyle": "square", "fc": "w", "lw": 2, "pad": 0.6},
                 arrowprops={'arrowstyle': '->'},
             )
             fig.canvas.draw_idle()
-
-def main(
-        df, x, category, hue=None, annotate=None,
-        on_hover=None, on_click=None, boxplot=True,
-        **kwargs
-        ):
-    category_values = sorted(list(df[category].dropna().unique()))
-    hue_values = None
-    if hue:
-        hue_values = sorted(list(df[hue].unique()))
-
-    fig, ax = plt.subplots()
-    if boxplot:
-        sns.boxplot(
-            data=df, x=x, y=category, hue=hue,
-            whis=(10, 90),
-            orient='h',
-            order=category_values,
-            hue_order=hue_values,
-            )
-    else:
-        df_sorted = pointplot(df, x, hue, kwargs.get('palette'))
- 
-    ax.set_title(kwargs.get('title', None))
-    vlines = kwargs.get('vlines', None)
-    if vlines:
-        _, ymax = plt.ylim()
-        for what, where in vlines.items():
-            ax.axvline(where, color='g')
-            #xticks.append(x)
-            #xlabels.append(labx)
-            ax.annotate(f"{what}", (where, ymax), rotation=0)
-    text = kwargs.get('text', None)
-    if text:
-        props = dict(boxstyle='round', facecolor='white', alpha=0.5)
-        ax.text(1.05, 0.50, text, transform=ax.transAxes, fontsize=14,
-            verticalalignment='top', bbox=props)
-
-
-    if annotate is not None:
-        if boxplot:
-            if on_hover is None:
-                on_hover = hover
-            on_hover._df = set_y(df, category, hue)
-            on_hover._df['x'] = df[x]
-        else:
-            on_hover._df = df_sorted
-            on_hover._df['y'] = df_sorted.x
-            on_hover._df['x'] = df_sorted.index
-            print(on_hover._df)
-        on_hover._annotate = annotate
-        id = fig.canvas.mpl_connect('motion_notify_event', on_hover)
-
-    on_click._df = on_hover._df
-    fig.canvas.mpl_connect('button_press_event', on_click)
-
-    plt.show()
-
-def set_y(df, y_label, y_sublabel=None):
-    _df = df.copy()
-    y_labels = sorted(list(_df[y_label].unique()))
-    y_values = [y_labels.index(row[y_label]) if pd.notnull(row[y_label])  else -1 for _, row in _df.iterrows() ]
-    _df["y"] = y_values
-    if y_sublabel:
-        y_sublabels = sorted(list(_df[y_sublabel].unique()))
-        y_subvalues = [
-            y_sublabels.index(row[y_sublabel]) for _, row in _df.iterrows()
-        ]
-        _df["dy"] = y_subvalues
-        _df.dy -= (len(y_sublabels)-1) / 2
-        _df.dy *= .4
-        _df.y += _df.dy
-    return _df
-
-def ignore(event):
-    pass
-
-def get_row(event, df):
-    if event.xdata is not None and event.ydata is not None:
-        is_near_x = (df.x - event.xdata)**2 < 10000
-        is_near_y = (df.y - event.ydata)**2 < 0.1
-        selected = is_near_x & is_near_y
-        if selected.any():
-            return df[selected].iloc[0]
-
-def get_row_pp(event, df):
-    if event.xdata is not None and event.ydata is not None:
-        #print("x values: ", df.x, event.xdata)
-        #print("y values: ", df.y, event.ydata)
-        is_near_x = (df.x - event.xdata)**2 < 0.5
-        is_near_y = (df.y - event.ydata)**2 < 10000
-        selected = is_near_x & is_near_y
-        if selected.any():
-            return df[selected].iloc[0]
-        
-def hover(event):
-    if True:
-        row = get_row(event, hover._df)
-    else:
-        row = get_row_pp(event, hover._df)
-    if row is not None:
-        fig = plt.gcf()
-        ax = plt.gca()
-        ax.annotate(
-            "\n".join(f"{row[k]}" for k in hover._annotate if pd.notnull(row[k]) and row[k] != 0),
-            xy=(row.x, row.y),
-            xytext=(20, 20),
-            textcoords="offset points",
-            bbox={"boxstyle": "square", "fc": "w", "lw": 2, "pad": 0.6},
-            arrowprops={'arrowstyle': '->'},
-        )
-        fig.canvas.draw_idle()
-
-
-def pointplot(df, y, hue, palette):
-    df_sorted = df.sort_values(y).reset_index(drop=True)
-    sns.stripplot(
-        data=df_sorted, y=y, x=df_sorted.index, hue=hue,
-        palette=palette
-    ).set_xticklabels("")
-    df_sorted['y'] = df[y]
-    return df_sorted
 
 def print_xy(event):
     print(event.xdata, event.ydata)
@@ -228,10 +104,9 @@ if __name__ == "__main__":
 
     df = pd.DataFrame(dict(x=x, km=km, school=school))
 
-
     pl = Plotter(
         df, 'x', category='school', hue='km',
-        on_hover=hover,
+        #on_hover=hover,
         on_click=print_xy,
         annotate=('school', 'x', 'km')
     )
--- test_click.py
+++ test_click.py
@@ -14,123 +14,126 @@ def df():
     #x = np.random.randint(20000, 40000, sample)
     #km = np.random.choice(['Kvinna', 'Man'], sample)
     #school = np.random.choice(['A', 'B', 'C'], sample)
-    return pd.DataFrame(
+    _df = pd.DataFrame(
         dict(
-            x=[22732, 30799, 29845, 39648, 33123,
-               29225, 34116, 34935, 35430, 35832],
+            kr=[22732, 30799, 29845, 39648, 33123,
+               29225, 34116, 34935, 35430, 35832, 0],
             km=['Kvinna', 'Kvinna', 'Kvinna', 'Man', 'Kvinna',
-                'Man', 'Man', 'Kvinna', 'Kvinna', 'Man'],
-            school=['B', 'B', 'B', 'A', 'B', 'A', 'A', 'B', 'C', 'A'],
+                'Man', 'Man', 'Kvinna', 'Kvinna', 'Man', 'Man'],
+            school=['B', 'B', 'B', 'A', 'B', 'A', 'A', 'B', 'C', 'A', 'A'],
         )
     )
+    return _df[_df.kr > 0]
    
 def test_setup(df):
-    plotter = Plotter(df, "x", "school")
+    plotter = Plotter(df, "kr", "school")
     assert plotter.df is df
-    assert plotter.x == "x"
+    assert plotter.x == "kr"
     assert plotter.category == "school"
 
+@pytest.fixture
+def plx(df):
+    return Plotter(df, "kr")
+
+def test_no_category_values(plx):
+    assert plx.category_values() == []
+    assert plx.hue_values() == []
+    pdt.assert_series_equal(plx.set_y(), pd.Series(np.zeros(len(plx.df))))
+
 def test_category_values(df):
-    plotter = Plotter(df, "x", "school")
+    plotter = Plotter(df, "kr", "school")
     assert plotter.category_values() == ["A", "B", "C"]
 
-def test_hue_values(df):
-    plotter = Plotter(df, "x", "school", "km")
-    assert plotter.hue_values() == ["Kvinna", "Man"]
+@pytest.fixture
+def plxy(df):
+    return Plotter(df, "kr", "school")
+
+@pytest.fixture
+def plxyz(df):
+    return Plotter(df, "kr", "school", "km")
+
+@pytest.fixture
+def plxzy(df):
+    return Plotter(df, "kr", "km", "school")
+
+def test_hue_values(plxyz):
+    assert plxyz.hue_values() == ["Kvinna", "Man"]
 
 @mock.patch('click.plt.show')
-def test_boxplot_subplots(mock_show, df):
-    plotter = Plotter(df, "x", "school", "km")
+def test_boxplot_subplots(mock_show, plxyz):
     with mock.patch('click.plt.subplots') as mockplots:
         mockplots.return_value = mock.Mock(), mock.Mock()
-        plotter.boxplot()
+        plxyz.boxplot()
         mockplots.assert_called_once()
 
 @mock.patch('click.plt.show')
-def test_boxplot_seaborn(mock_show, df):
-    plotter = Plotter(df, "x", "school", "km")
+def test_boxplot_seaborn(mock_show, plxyz):
     with mock.patch('click.sns.boxplot') as mockplot:
-        plotter.boxplot()
+        plxyz.boxplot()
         mockplot.assert_called_once_with(
-            data=plotter.df, x="x", y="school", hue="km",
+            data=plxyz.df, x="kr", y="school", hue="km",
             whis=(10,90), order=["A", "B", "C"], 
             hue_order=["Kvinna", "Man"]
         )
 
-def test_boxplot_show(df):
-    plotter = Plotter(df, "x", "school", "km")
+def test_boxplot_show(plxyz):
     with mock.patch('click.plt.show') as mockshow:
-        plotter.boxplot()
+        plxyz.boxplot()
         mockshow.assert_called_once()
 
 def test_hover(df):
     hover = lambda: None
-    plotter = Plotter(df, "x", "school", "km", on_hover=hover)
+    plotter = Plotter(df, "kr", "school", "km", on_hover=hover)
     assert plotter.on_hover == hover
 
 @mock.patch('click.plt.show')
-def test_hover_connect(mock_show, df):
+def test_hover_connect(mock_show, plxyz):
     with mock.patch('click.plt.subplots') as mock_plots:
         mock_fig = mock.MagicMock()
         mock_ax = mock.MagicMock()
         mock_plots.return_value = mock_fig, mock_ax
-        plotter = Plotter(df, "x", "school", "km")
-        plotter.boxplot()
-        mock_fig.canvas.mpl_connect.assert_called_with('motion_notify_event', plotter)
+        plxyz.boxplot()
+        mock_fig.canvas.mpl_connect.assert_called_with('motion_notify_event', plxyz)
 
 def test_annotate(df):
-    plotter = Plotter(df, "x", "school", "km", annotate=('Skola', 'x'))
-    assert plotter.annotate == ('Skola', 'x')
+    plotter = Plotter(df, "kr", "school", "km", annotate=('Skola', 'kr'))
+    assert plotter.annotate == ('Skola', 'kr')
 
-def test_get_row_1(df):
-    plotter = Plotter(df, "x")
-    row = plotter.get_row(Event(30799, 0))
-    pdt.assert_series_equal(row, df.loc[1])
+def test_get_row_1(plx):
+    row = plx.get_row(Event(30799, 0))
+    pdt.assert_series_equal(row, plx.df.loc[1])
 
-def test_get_row_2(df):
-    plotter = Plotter(df, "x", "school")
-    row = plotter.get_row(Event(35832, 0))
-    pdt.assert_series_equal(row, df.loc[9])
+def test_get_row_2(plxy):
+    row = plxy.get_row(Event(35832, 0))
+    pdt.assert_series_equal(row, plxy.df.loc[9])
 
-#@pytest.mark.skip()
-def test_get_row_2m(df):
-    plotter = Plotter(df, "x", "school", "km")
-    row = plotter.get_row(Event(35832, 0.2))
-    print(df)
-    pdt.assert_series_equal(row, df.loc[9])
+def test_get_row_2m(plxyz):
+    row = plxyz.get_row(Event(35832, 0.2))
+    pdt.assert_series_equal(row, plxyz.df.loc[9])
 
-def test_get_row_3(df):
-    plotter = Plotter(df, "x", "school")
-    row = plotter.get_row(Event(22732, 1))
-    pdt.assert_series_equal(row, df.loc[0])
+def test_get_row_3(plxy):
+    row = plxy.get_row(Event(22732, 1))
+    pdt.assert_series_equal(row, plxy.df.loc[0])
 
-def test_get_row_4(df):
-    plotter = Plotter(df, "x", "school")
-    row = plotter.get_row(Event(35430, 2))
-    pdt.assert_series_equal(row, df.loc[8])
+def test_get_row_4(plxy):
+    row = plxy.get_row(Event(35430, 2))
+    pdt.assert_series_equal(row, plxy.df.loc[8])
 
-def test_set_y(df):
-    plotter = Plotter(df, 'x')
-    assert plotter.set_y() == [0]
-
-def test_y_shift_2(df):
-    plotter = Plotter(df, 'x', 'school', 'km')
-    pdt.assert_series_equal(plotter.y_shift(), pd.Series([
+def test_y_shift_2(plxyz):
+    pdt.assert_series_equal(plxyz.y_shift(), pd.Series([
         -.2, -.2, -.2, .2, -.2, 
         .2, .2, -.2, -.2, .2
         ])
     )
 
-def test_y_shift_3(df):
-    plotter = Plotter(df, 'x', 'km', 'school')
-    pdt.assert_series_equal(plotter.y_shift(), pd.Series([
+def test_y_shift_3(plxzy):
+    pdt.assert_series_equal(plxzy.y_shift(), pd.Series([
         0, 0, 0, -.8/3, 0, 
         -.8/3, -.8/3, 0, .8/3, -.8/3
         ])
     )
 
-def test_no_hue(df):
-    plotter = Plotter(df, 'x', 'km')
-    pdt.assert_series_equal(plotter.y_shift(), pd.Series(np.zeros(10)))
+def test_no_hue(plxy):
+    pdt.assert_series_equal(plxy.y_shift(), pd.Series(np.zeros(10)))
 
 
