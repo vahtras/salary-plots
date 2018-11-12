0c3f2e1df015c7f24b0d71e0263668c1cddd0d6b
Olav Vahtras <vahtras@kth.se>
2018-11-12 09:59:39 +0100

    update tests

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -5,17 +5,17 @@ from collections import namedtuple
 import pandas as pd
 import pandas.testing as pdt
 
-from catplot import plotters, main
+from catplot import plotters, main, util
 
 Event = namedtuple('event', ['xdata', 'ydata'])
    
 def test_get_0(df):
-    calculated = main.process_filters(df, ("kr=0",))
+    calculated = util.process_filters(df, ("kr=0",))
     expected = pd.DataFrame(dict(kr=[0], km=['Barn'], school=['D']), index=[5])
     pdt.assert_frame_equal(calculated, expected)
 
 def test_rm_0a(df):
-    calculated = main.process_filters(df, ("kr!=0",))
+    calculated = util.process_filters(df, ("kr!=0",))
     expected = df[df.kr > 0]
     pdt.assert_frame_equal(calculated, expected)
 
@@ -25,11 +25,11 @@ def test_rm_0b(df):
     pdt.assert_frame_equal(calculated, expected)
 
 def test_rm_0c(df):
-    calculated = main.process_filters(df, ("kr<25000",))
+    calculated = util.process_filters(df, ("kr<25000",))
     expected = df[df.kr < 25000]
     pdt.assert_frame_equal(calculated, expected)
 
 def test_isin(df):
-    calculated = main.process_filters(df, ("school in C D",))
+    calculated = util.process_filters(df, ("school in C D",))
     expected = df[df.school.isin(['C','D'])]
     pdt.assert_frame_equal(calculated, expected)
