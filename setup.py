a81b6c8e0a21e9d6d7b58edb71fa1921b1d0f752
Olav Vahtras <vahtras@kth.se>
2019-11-06 18:36:42 +0100

    include xlrd

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="catplot",
     packages=["catplot"],
-    install_requires=["pandas", "seaborn"],
+    install_requires=["pandas", "seaborn", "xlrd"],
     scripts=[
         "scripts/boxplot",
         "scripts/boxplot-demo",
