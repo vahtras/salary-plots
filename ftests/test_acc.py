65bbf94cb9d334150280b2a8070cc26c4a651b41
Olav Vahtras <vahtras@kth.se>
2024-10-29 16:43:48 +0100

    cleanup in ftests

--- ftests/test_acc.py
+++ ftests/test_acc.py
@@ -1,10 +1,12 @@
+from pathlib import Path
 import sys
 import unittest.mock as mock
 from catplot.main import main
 
+import pytest
 
-@mock.patch("catplot.main.plt.show")
-def test_boxplot(mock_show):
+@pytest.fixture
+def cleanup():
     sys.argv[1:] = [
         "--data",
         "ftests/sample.csv",
@@ -12,34 +14,28 @@ def test_boxplot(mock_show):
         "Lon",
         "--cat",
         "Cat",
-        "--plot-type",
-        "box",
         "--title",
         "POSTDOKTOR",
         "--table",
         "0",
     ]
+    yield
+    Path('tab-Lon-Cat.csv').unlink()
+    Path('tab-Lon-Cat.xlsx').unlink()
+
+@mock.patch("catplot.main.plt.gcf")
+@mock.patch("catplot.main.plt.show")
+def test_boxplot(mock_show, mock_gcf, cleanup):
     with mock.patch("catplot.plotters.sns.boxplot") as mock_plot:
+        sys.argv.extend(["--plot-type", "box"])
         main()
         assert mock_plot.called
 
 
+@mock.patch("catplot.main.plt.gcf")
 @mock.patch("catplot.main.plt.show")
-def test_pointplot(mock_show):
-    sys.argv[1:] = [
-        "--data",
-        "ftests/sample.csv",
-        "--num",
-        "Lon",
-        "--cat",
-        "Cat",
-        "--plot-type",
-        "point",
-        "--title",
-        "POSTDOKTOR",
-        "--table",
-        "0",
-    ]
+def test_pointplot(mock_show, mock_gcf, cleanup):
     with mock.patch("catplot.plotters.sns.stripplot") as mock_plot:
+        sys.argv.extend(["--plot-type", "point"])
         main()
         assert mock_plot.called
